c49ee4c33ed95e6b75e5704ced2f35f912e2d4ee
Devid <devidparmar111@gmail.com>
2026-06-30 20:21:44 +0530

    Add files via upload

---
+++ PR_04/PR_04/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PR_04
+{
+
+    class Person
+    {
+        string Name;
+        int Age;
+
+        public void ShowDetails(string name , int age)
+        {
+            Name = name;
+            Age = age;
+
+            Console.WriteLine($"Name : {Name} ");
+            Console.WriteLine($"Age : {Age} ");
+        }
+
+
+    }
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter you name : ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Age : ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Person a1;
+
+            a1 = new Person();
+            a1.ShowDetails(name, age);
+
+        }
+    }
+}
