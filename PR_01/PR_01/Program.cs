dc3e9081f905bbcb78e7c2950ad831e480c41868
Devid <devidparmar111@gmail.com>
2026-07-02 20:38:15 +0530

    Add files via upload

---
+++ PR_01/PR_01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PR_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Number: ");
+            int roll = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter City :");
+            string city = Console.ReadLine();
+
+
+            Console.WriteLine($"Roll number : {roll}");
+            Console.WriteLine($"Name : {name}");
+            Console.WriteLine($"City : {city}");
+            Console.WriteLine($"Age : {age}");
+
+            Console.WriteLine();
+
+        }
+    }
+}
